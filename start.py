57098ed35bed8a8c13c6ebe32b50d5800e332d34
Agentrodgers008 <varadarajannidish@gmail.com>
2025-08-10 17:48:54 +0530

    third commit

--- start.py
+++ start.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import json
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/workouts")
+def read_workouts():
+    with open("data.json", "r") as f:
+        data=json.load(f)
+        return data
