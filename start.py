3e4f0e1046789334390e809c9ba1a1966950d521
Agentrodgers008 <varadarajannidish@gmail.com>
2025-08-11 21:19:53 +0530

    Post method, path parameter, and query paraameter studied

--- start.py
+++ start.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,Query
+from typing import Optional
 from pydantic import BaseModel
 import json
 app = FastAPI()
+
+class Workout(BaseModel):
+    name: str
+    exercises: list[str]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/workouts")
-def read_workouts():
-    with open("data.json", "r") as f:
-        data=json.load(f)
-        return data
+def show_wkts(workoutname: Optional[str] = None):
+    with open("data.json", "r") as data_show:
+        show = json.load(data_show)
+        if workoutname:
+            if workoutname in show:
+                return show[workoutname]
+            else:
+                return {"Error": "Workout not Found"}
+        return show
+@app.post("/workouts")
+def create_workout(workout: Workout):
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    data[workout.name] = workout.exercises
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+    return {"message": "Workout created successfully", "workout": workout}
