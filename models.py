92ae8c3d9b8ec6299903e835ed3ea22e5ac579ce
Agentrodgers008 <varadarajannidish@gmail.com>
2025-09-05 15:22:17 +0530

    database connecrion successful

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from sqlmodel import Column, Integer, String, BOOLEAN, SQLModel,Field
-import pymysql as pym
-from typing import Optional,Union
+from sqlmodel import Column, Integer, String,SQLModel,Field, Date
+from typing import Optional
 from pydantic import EmailStr
 from datetime import date
+
 
 #class Workout(SQLModel,table =True):
     
@@ -11,14 +11,13 @@ from datetime import date
 class User(SQLModel,table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    email: EmailStr = Field(sa_column=Column("email", String, unique=True, index=True))
-    password: str = Field(sa_column=Column(String, nullable=False))
+    email: EmailStr = Field(sa_column=Column(String(255), unique=True, index=True))
+    password: str = Field(sa_column=Column(String(255), nullable=False))
     
 
 
-class UpdateWorkout(SQLModel):
-    name: str
-    exercise= Field(String)
-    exercise_date: date=Field(String)
-    sets: int=Field(Integer)
-    reps: int=Field(Integer)
+class UpdateWorkout(SQLModel, table=True):
+    
+    exercise: str= Field(sa_column=Column(String(255), nullable=False,primary_key=True))
+    exercise_date: date=Field(default_factory=date.today(),sa_column=Column(Date,nullable=False))
+    sets: int=Field(sa_column=Column(Integer, nullable=False))
