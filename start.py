b9a49e8a2e702ae7191d63584e37274c1ee99901
Agentrodgers008 <varadarajannidish@gmail.com>
2025-10-16 22:42:37 +0530

    made the sign in and sign up enpoints working

--- start.py
+++ start.py
@@ -1,14 +1,10 @@
-from fastapi import FastAPI,Request
-from typing import Optional,Union
+from fastapi import FastAPI,Request,Depends,HTTPException
 from pydantic import EmailStr
 from sqlmodel import select,Session
-import json
-from dotenv import load_dotenv
-import os
 import datetime
-from models import UpdateWorkout
+from models import UpdateWorkout, User
 from database import engine
-load_dotenv()
+import bcrypt
 app = FastAPI()
 
 
@@ -18,6 +14,33 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/Sign in")
+def Sign_in(email:EmailStr, password:str):
+    try:
+        with Session(engine) as session:
+            statement = select(User).where(User.email==email)
+            result = session.exec(statement).first()
+            if result is None:
+                raise HTTPException(status_code=404, detail="User not found")
+            if not bcrypt.checkpw(password.encode('utf-8'),result.password.encode('utf-8')):
+                    raise HTTPException(status_code=400, detail="Incorrect password")
+            
+            return {"message": "Login successful"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+
+
+@app.post("/Sign_up")
+def Sign_up(name:str,email:EmailStr,password:str):
+    password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    try:
+        with Session(engine) as session:
+            user=User(name=name,email=email,password=password)
+            session.add(user)
+            session.commit()
+            return {"message": "User created succssfully"}
+    except HTTPException as e:
+        raise HTTPException(status_code=400, detail="Email already exists")
 @app.get("/workouts")
 def show_wkts():
     try:
@@ -38,4 +61,5 @@ def create_workout(workout: UpdateWorkout):
             return {"message": "Workout added successfully"}
     except Exception as e:
         return {"error": str(e)}
+    
             
